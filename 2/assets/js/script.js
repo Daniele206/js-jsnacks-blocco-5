fd6926fe8c567ef6f5bd84f295708c21cdae767f
Daniele206 <danieledeieso2004@gmail.com>
2024-03-07 15:53:36 +0100

    [finish] snack2

--- 2/assets/js/script.js
+++ 2/assets/js/script.js
@@ -1,52 +1,90 @@
 const zucchine = [
   {
     varietá: 'nera',
-    perso_in_g: 100,
+    peso_in_g: 100,
     lunghezza_in_cm: 14,
   },
   {
     varietá: 'napoletana',
-    perso_in_g: 225,
+    peso_in_g: 225,
     lunghezza_in_cm: 21,
   },
   {
     varietá: 'tonda',
-    perso_in_g: 150,
+    peso_in_g: 150,
     lunghezza_in_cm: 16,
   },
   {
     varietá: 'trombetta',
-    perso_in_g: 300,
+    peso_in_g: 300,
     lunghezza_in_cm: 26,
   },
   {
     varietá: 'gialla',
-    perso_in_g: 250,
+    peso_in_g: 250,
     lunghezza_in_cm: 24,
   },
   {
     varietá: 'fiorentina',
-    perso_in_g: 50,
+    peso_in_g: 50,
     lunghezza_in_cm: 7,
   },
   {
     varietá: 'romanesca',
-    perso_in_g: 80,
+    peso_in_g: 80,
     lunghezza_in_cm: 9,
   },
   {
     varietá: 'patisson',
-    perso_in_g: 450,
+    peso_in_g: 450,
     lunghezza_in_cm: 35,
   },
   {
     varietá: 'rugoso friulano',
-    perso_in_g: 200,
+    peso_in_g: 200,
     lunghezza_in_cm: 19,
   },
   {
     varietá: 'crookneck',
-    perso_in_g: 170,
+    peso_in_g: 170,
     lunghezza_in_cm: 16,
   },
 ];
+
+const zucchineGrandi = zucchine.filter(zucchinaGrande);
+const zucchinePiccole = zucchine.filter(zucchinaPiccola);
+
+function zucchinaGrande(zucchina){
+  return zucchina.lunghezza_in_cm > 15;
+};
+
+function zucchinaPiccola(zucchina){
+  return zucchina.lunghezza_in_cm < 15;
+};
+
+console.log(zucchineGrandi); 
+
+let sommaZucchineGrandi = 0;
+
+for(zucchinaG of zucchineGrandi){
+  sommaZucchineGrandi = sommaZucchineGrandi + zucchinaG.peso_in_g
+}
+
+console.log(sommaZucchineGrandi);
+
+//-----------------------------------------------
+
+console.log(zucchinePiccole); 
+
+let sommaZucchinePiccole = 0;
+
+for(zucchinaP of zucchinePiccole){
+  sommaZucchinePiccole = sommaZucchinePiccole + zucchinaP.peso_in_g
+}
+
+console.log(sommaZucchinePiccole);
+
+
+const output = document.querySelector('.my-output');
+
+output.innerHTML = 'Le zucchine grandi pesano insieme ' + sommaZucchineGrandi + 'g ' + 'e quelle piccole ' + sommaZucchinePiccole + 'g';
