3f2216fd020a18fa4eb1ea24c1c88128244e2a68
Daniele206 <danieledeieso2004@gmail.com>
2024-03-07 17:11:47 +0100

    [fix] some ditalis and [add] promt for number

--- 5/assets/js/script.js
+++ 5/assets/js/script.js
@@ -11,12 +11,29 @@ const arrayIniziale = [
   'Edoardo'
 ];
 
-const firstNumber = 2;
-const secondNumber = 7;
+let firstNumber;
+let firstNumberOk = 'notOk';
+let secondNumber;
+let secondNumberOk = 'notOk';
+
+while(firstNumberOk != 'ok' || secondNumberOk != 'ok'){
+  if(firstNumberOk === 'notOk'){
+    firstNumber = prompt('isnerisci un numero da 0 a ' + arrayIniziale.length);
+  }
+  if(firstNumber >= 0 && firstNumber <= arrayIniziale.length){
+    firstNumberOk = 'ok';
+    secondNumber = prompt('isnerisci un numero da ' + firstNumber + ' a ' + arrayIniziale.length);
+    
+    if(secondNumber >= firstNumber && secondNumber <= arrayIniziale.length){
+      secondNumberOk = 'ok';
+    }
+  };
+};
+
 
 const arrayFinale = [];
 
-for(let i = firstNumber - 1; i <= secondNumber - firstNumber + 1; i++){
+for(let i = firstNumber - 1; i <= secondNumber - 1; i++){
   arrayFinale.push(arrayIniziale[i]);
 };
 
@@ -24,4 +41,4 @@ console.log(arrayFinale);
 
 const output = document.querySelector('.my-output');
 
-output.innerHTML = 'I nome della lista compresi tra i numeri che hai inserito cono: ' + arrayFinale;
+output.innerHTML = 'I nomi della lista compresi tra i numeri che hai inserito cono: ' + arrayFinale;
