05fd79f110fccf3314418535a3678d6c406bc3d7
Daniele206 <danieledeieso2004@gmail.com>
2024-03-07 15:40:07 +0100

    [add] readme snack2

--- 2/README.md
+++ 2/README.md
@@ -6,3 +6,8 @@ snack2
 Crea 10 oggetti che rappresentano una zucchina.
 Dividi in due array separati le zucchine che misurano meno o più di 15cm.
 Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+## Svolgimento
+1. Creare un array con 10 oggetti.
+2. Andare a dividere questo array in due in base a una proprietá degli oggetti.
+3. Sommare i pesi delle zucchine dei due array finali per ottenere 2 pesi finali.
---
+++ 2/assets/js/script.js
@@ -0,0 +1,52 @@
+const zucchine = [
+  {
+    varietá: 'nera',
+    perso_in_g: 100,
+    lunghezza_in_cm: 14,
+  },
+  {
+    varietá: 'napoletana',
+    perso_in_g: 225,
+    lunghezza_in_cm: 21,
+  },
+  {
+    varietá: 'tonda',
+    perso_in_g: 150,
+    lunghezza_in_cm: 16,
+  },
+  {
+    varietá: 'trombetta',
+    perso_in_g: 300,
+    lunghezza_in_cm: 26,
+  },
+  {
+    varietá: 'gialla',
+    perso_in_g: 250,
+    lunghezza_in_cm: 24,
+  },
+  {
+    varietá: 'fiorentina',
+    perso_in_g: 50,
+    lunghezza_in_cm: 7,
+  },
+  {
+    varietá: 'romanesca',
+    perso_in_g: 80,
+    lunghezza_in_cm: 9,
+  },
+  {
+    varietá: 'patisson',
+    perso_in_g: 450,
+    lunghezza_in_cm: 35,
+  },
+  {
+    varietá: 'rugoso friulano',
+    perso_in_g: 200,
+    lunghezza_in_cm: 19,
+  },
+  {
+    varietá: 'crookneck',
+    perso_in_g: 170,
+    lunghezza_in_cm: 16,
+  },
+];
