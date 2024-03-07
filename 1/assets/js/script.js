81233e85f73457ab794cdf6efda2e397fd8f2898
Daniele206 <danieledeieso2004@gmail.com>
2024-03-07 15:25:07 +0100

    [add] array zucchine

---
+++ 1/assets/js/script.js
@@ -0,0 +1,37 @@
+const list = [
+  {
+    varietá: 'nera',
+    perso_in_g: 100,
+    lunghezza_in_cm: 14,
+  },
+  {
+    varietá: 'napoletana',
+    perso_in_g: 225,
+    lunghezza_in_cm: 21,
+  },
+  {
+    varietá: 'tonda',
+    perso_in_g: 150,
+    lunghezza_in_cm: 16,
+  },
+  {
+    varietá: 'trombetta',
+    perso_in_g: 300,
+    lunghezza_in_cm: 26,
+  },
+  {
+    varietá: 'gialla',
+    perso_in_g: 250,
+    lunghezza_in_cm: 24,
+  },
+  {
+    varietá: 'fiorentina',
+    perso_in_g: 50,
+    lunghezza_in_cm: 7,
+  },
+  {
+    varietá: 'romanesca',
+    perso_in_g: 80,
+    lunghezza_in_cm: 9,
+  },
+]
